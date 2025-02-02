074d1c2ba55f0da3ac88c2e15d3c4f0342cb0547
fatuousnerd <110926333+Fatuousnerd@users.noreply.github.com>
2025-02-02 13:09:01 +0300

    Renovation 02/02/25

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import './index.css';
+
 const App = () => {
-  return <div>Hello World</div>;
+  return (
+    <div className="font-extrabold text-5xl text-center mt-20 uppercase">
+      React Starter
+    </div>
+  )
 };
 
 export default App;
